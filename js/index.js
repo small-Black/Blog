63cc1404393c9e8384ac56d9178867f5f2851118
Xiao-Hei <649925003@qq.com>
2015-07-13 01:48:23 +0800

    增加分享功能，文章列表页

--- js/index.js
+++ js/index.js
@@ -1,29 +1,10 @@
 (function(window, $) {
     $(function() {
-        var $nav = $(".nav"),
-            $allsecondary = $(".secondary"),
-            $logo = $(".logo");
-        $nav.on("mouseover", " li", function() {
-            var $this = $(this),
-                id = $this.attr("id"),
-                $secondary = $this.children(".secondary");
-            $secondary.slideDown('500');
-            $secondary.children('a').addClass('action');
-            $this.children('.nav-title').addClass('on');
-
-
-        });
-        $nav.on("mouseleave", " li", function() {
-            var $this = $(this),
-                id = $this.attr("id");
-            $allsecondary.hide().stop(false, true);
-            $this.children('.nav-title').removeClass('on');
-            $this.children('.secondary').find('a').removeClass('action');
-        });
+        // 轮播图
         $.PicSlide({
             selector: ".pic", //选择器
             speed: 3000, //速度
-            dot: true, //焦点
+            dot: false, //焦点
             autoplay: true, //是否自动播放
             button: true //是否显示左右箭头
         });
