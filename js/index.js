fb66e3f5d6fdeb5195fd575c797bc60f8e9f141a
small-Black <649925003@qq.com>
2015-07-16 16:56:51 +0800

    增加文章详情

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@
             selector: ".pic", //选择器
             speed: 3000, //速度
             dot: false, //焦点
-            autoplay: true, //是否自动播放
-            button: true //是否显示左右箭头
+            autoplay: false, //是否自动播放
+            button: false //是否显示左右箭头
         });
     });
 })(window, jQuery);
